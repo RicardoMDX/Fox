c162e41812cf01ee30b085b1395babb205d58641
Ricardo <rh845@live.mdx.ac.uk>
2020-01-26 20:56:38 +0000

    V0.21
    Item highlighting

---
+++ Assets/CheckForItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForItem : MonoBehaviour
+{
+    private Camera camera;
+    private GameObject lastObject=null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 3f))
+        {
+            if(hit.transform.tag=="Item")
+            {
+                hit.transform.SendMessage("HighlightItem");
+                lastObject = hit.transform.gameObject;
+            }
+        }
+        else if(lastObject!=null)
+        {
+            lastObject.SendMessage("DehighlightItem");
+        }
+    }
+}
---
+++ Assets/Highlight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour
+{
+    private Color startcolor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HighlightItem()
+    {
+        startcolor = GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    public void DehighlightItem()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
